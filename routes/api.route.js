4aaf61e8611f6b9aabfc01365271a3779b6c4a84
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-08-11 17:12:23 +0100

    :sparkles: feat: Added the qr code routes

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
+const qrCodeController = require('../controllers/qrcode.controller');
+
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Welcome to Gen_code api 🚀' });
@@ -11,6 +13,9 @@ router.post('/login', authController.signInUser);
 
 
 //Qrcode Routes
+// POST: Save QR Code
+router.post('/save-qr-code', qrCodeController.saveQRCode);
+router.get('/qr-codes/:userId', qrCodeController.getUserQRCodes);
 
 
 module.exports = router;
